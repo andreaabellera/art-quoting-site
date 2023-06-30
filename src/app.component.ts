daa81a7c4268e009a76711b029f92ebb1efc1faf
Andrea Abellera <andrea.abellera@gmail.com>
2023-06-30 14:53:40 -0500

    One small dot for the code, one giant leap for the website

--- src/app.component.ts
+++ src/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-root',
   template: `
-    <painting src="/painting.jpg" title="Chocolate strawberry cake"></painting>
+    <painting src="./painting.jpg" title="Chocolate strawberry cake"></painting>
   `
 })
 export class AppComponent {}
